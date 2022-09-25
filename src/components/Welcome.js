c3b4e463ffdb7482ffd0d267d369b54301834269
Divyansh Dubey <108198974+divyanshdube@users.noreply.github.com>
2022-09-25 18:37:51 +0530

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,9 +1,9 @@
 function Welcome(props) {
   return (
     <div>
-      <details>
+      
         <h1>Hey ! {props.name}</h1>
-      </details>
+      
       <h2>Welcome to Newton School.</h2>
     </div>
   );
